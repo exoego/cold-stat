2356be70792c53fd02c36d47bb9d8b68675f765e
exoego <ytatsuno.jp@gmail.com>
2024-03-20 19:43:52 +0900

    init

---
+++ src/lambda_invoker.rs
@@ -0,0 +1,138 @@
+use std::collections::HashMap;
+use std::time::Duration;
+use anyhow::anyhow;
+use aws_sdk_lambda as lambda;
+use aws_sdk_lambda::operation::get_function::GetFunctionOutput;
+use aws_sdk_lambda::operation::get_function_configuration::GetFunctionConfigurationOutput;
+use aws_sdk_lambda::primitives::Blob;
+use aws_sdk_lambda::types::{Environment, LastUpdateStatus, State};
+use log::{info, warn};
+use uuid::Uuid;
+
+pub struct LambdaInvoker {
+    lambda_client: lambda::Client,
+    function_name: String,
+    payload: Blob,
+}
+
+impl LambdaInvoker {
+    pub fn new(
+        lambda_client: lambda::Client,
+        function_name: String,
+        payload: String,
+    ) -> Self {
+        Self {
+            lambda_client,
+            function_name,
+            payload: Blob::new(payload.into_bytes()),
+        }
+    }
+
+    pub async fn iterate(&self, iterations: u8) -> Result<(), anyhow::Error> {
+        let config = self.get_function_configuration().await?;
+        let mut env = config.environment.map(|a| a.variables.unwrap_or_default()).unwrap_or_default();
+
+        for _ in 0..iterations {
+            env.insert("cold_start_uuid".to_string(), Uuid::new_v4().to_string());
+            self.refresh_lambda(env.clone()).await?;
+            self.wait_for_function_ready().await?;
+            self.invoke().await?;
+        }
+        Ok(())
+    }
+
+    async fn invoke(&self) -> Result<(), anyhow::Error> {
+        info!("Invoking function");
+        let result = self.lambda_client
+            .invoke()
+            .function_name(self.function_name.clone())
+            .payload(self.payload.clone())
+            .send()
+            .await?;
+        info!("Function error {:?}", result.function_error);
+        Ok(())
+    }
+
+    async fn get_function_configuration(&self) -> Result<GetFunctionConfigurationOutput, anyhow::Error> {
+        info!("Getting function configuration");
+        self.lambda_client
+            .get_function_configuration()
+            .function_name(self.function_name.to_string())
+            .send()
+            .await
+            .map_err(anyhow::Error::from)
+    }
+
+    async fn get_function(&self) -> Result<GetFunctionOutput, anyhow::Error> {
+        info!("Getting function");
+        self.lambda_client
+            .get_function()
+            .function_name(self.function_name.clone())
+            .send()
+            .await
+            .map_err(anyhow::Error::from)
+    }
+
+    async fn refresh_lambda(&self, env: HashMap<String, String>) -> Result<(), anyhow::Error> {
+        self.lambda_client
+            .update_function_configuration()
+            .function_name(self.function_name.to_string())
+            .environment(Environment::builder().set_variables(Some(env.clone())).build()).send()
+            .await
+            .map(|_| ())
+            .map_err(anyhow::Error::from)
+    }
+
+    async fn wait_for_function_ready(&self) -> Result<(), anyhow::Error> {
+        info!("Waiting for function");
+        while !self.is_function_ready().await? {
+            info!("Function is not ready, sleeping 1s");
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        }
+        Ok(())
+    }
+
+    async fn is_function_ready(
+        &self,
+    ) -> Result<bool, anyhow::Error> {
+        match self.get_function().await {
+            Ok(func) => {
+                if let Some(config) = func.configuration() {
+                    if let Some(state) = config.state() {
+                        info!("Checking if function is active: {state}");
+                        if !matches!(state, State::Active) {
+                            return Ok(false);
+                        }
+                    }
+                    match config.last_update_status() {
+                        Some(last_update_status) => {
+                            info!("Checking if function is ready: {last_update_status}");
+                            match last_update_status {
+                                LastUpdateStatus::Successful => {
+                                    return Ok(true);
+                                }
+                                LastUpdateStatus::Failed | LastUpdateStatus::InProgress => {
+                                    return Ok(false);
+                                }
+                                unknown => {
+                                    warn!("LastUpdateStatus unknown: {unknown}");
+                                    return Err(anyhow!(
+                                        "Unknown LastUpdateStatus, fn config is {config:?}"
+                                    ));
+                                }
+                            }
+                        }
+                        None => {
+                            warn!("Missing last update status");
+                            return Ok(false);
+                        }
+                    };
+                }
+            }
+            Err(e) => {
+                warn!("Could not get function while waiting: {e}");
+            }
+        }
+        Ok(false)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+mod lambda_invoker;
+
+use aws_config::BehaviorVersion;
+use aws_sdk_lambda::Client;
+use clap::Parser;
+use log::{info, LevelFilter};
+use simple_logger::SimpleLogger;
+use crate::lambda_invoker::LambdaInvoker;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    function: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    iterations: u8,
+
+    #[arg(short, long)]
+    payload: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let Args {
+        function,
+        iterations,
+        payload,
+    } = Args::parse();
+
+    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
+
+    let config = aws_config::load_defaults(BehaviorVersion::v2023_11_09()).await;
+    let lambda = Client::new(&config);
+    let lambda_invoker = LambdaInvoker::new(lambda.clone(), function.clone(), payload);
+
+    lambda_invoker.iterate(iterations).await?;
+
+    info!("Done");
+    Ok(())
+}
